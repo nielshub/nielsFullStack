063319f8bbabf40caad82737256132ef15fd5bc1
nielshub <niels.sanchez@gmail.com>
2020-03-22 21:58:33 +0100

    Adding classes

--- Proyecto_2/Index.mjs
+++ Proyecto_2/Index.mjs
@@ -1,4 +1,4 @@
-import * as CardFuncs from './CardFuncs.mjs';
+import * as CardFuncs from "./CardFuncs.mjs";
 
 //Added functionality to arrays to remove repeated cards
 Array.prototype.unique = (function(a) {
@@ -21,6 +21,25 @@ let checkwinner = document.querySelector("#checkwinner");
 let Player = [];
 let Cards = [];
 let CardShowup = [];
+let CardShowTable = [];
+
+class ClassPlayer {
+  constructor(name, Cards) {
+    this.name = name;
+    this.Card1 = CardFuncs.getRandomInt(0, 51, Cards);
+    Cards.push(this.Card1);
+    this.Card2 = CardFuncs.getRandomInt(0, 51, Cards);
+    Cards.push(this.Card2);
+  }
+}
+
+class TableCards {
+  constructor(name, Cards) {
+    this.name = name;
+    this.Card = CardFuncs.getRandomInt(0, 51, Cards);
+    Cards.push(this.Card);
+  }
+}
 
 //Clean the table
 CardFuncs.removeChildren(CardZone);
@@ -29,18 +48,13 @@ CardFuncs.removeChildren(CardTable);
 //Players button -> Deal cards to the players & add 5 unshown cards to the table
 form.addEventListener("submit", event => {
   event.preventDefault();
+  Cards = [];
   let gameplayers = Number(players.value);
   CardFuncs.removeChildren(CardZone);
   CardFuncs.removeChildren(CardTable);
   CardFuncs.removeChildren(Results);
   for (let i = 0; i < gameplayers; i++) {
-    Player[i] = {
-      name: i + 1,
-      Card1: CardFuncs.getRandomInt(0, 51, Cards),
-      Card2: CardFuncs.getRandomInt(0, 51, Cards)
-    };
-    Cards[i * 2] = Player[i].Card1;
-    Cards[i * 2 + 1] = Player[i].Card2;
+    Player[i] = new ClassPlayer(i + 1, Cards);
     let CardHand = document.createElement("div");
     CardHand.className = "CardHand";
     CardHand.innerHTML = `
@@ -83,33 +97,32 @@ form.addEventListener("submit", event => {
                 width="150">
             </div>`;
     CardFuncs.addCards(CardTable, CardShowdown);
+    CardShowup[i] = new TableCards(i + 1, Cards);
   }
 });
 
 //Check button -> Unshow 5 table cards & check the results for each player
 formCheck.addEventListener("submit", event => {
   event.preventDefault();
-  let gameplayers = Number(players.value);
   CardFuncs.removeChildren(CardTable);
   CardFuncs.removeChildren(Results);
   for (let i = 0; i < 5; i++) {
-    CardShowup[i] = CardFuncs.getRandomInt(0, 51, Cards);
-    Cards.push(CardShowup[i]);
     let CardShow = document.createElement("div");
     CardShow.className = "CardShowdown";
     CardShow.innerHTML = `
             <div>
                     <img src="cartas/JPEG/${CardFuncs.TranslatorCard.CardNumber(
-                      CardShowup[i]
+                      CardShowup[i].Card
                     )}${CardFuncs.TranslatorCard.FamilyEnglish(
-      CardShowup[i]
+      CardShowup[i].Card
     )}.jpg"
                     height="250"
                     width="150">
             </div>`;
     CardFuncs.addCards(CardTable, CardShow);
+    CardShowTable.push(CardShowup[i].Card);
   }
-  for (let i = 0; i < gameplayers; i++) {
+  for (let i = 0; i < Player.length; i++) {
     let HandResult = document.createElement("div");
     HandResult.className = "CardHand";
     HandResult.innerHTML = `
@@ -119,7 +132,7 @@ formCheck.addEventListener("submit", event => {
             }</p>
             <p style="text-align: center;">${CardFuncs.CalculateScore(
               Player[i],
-              CardShowup
+              CardShowTable
             )}</p>
             </div>
             `;
